309a691c4638791653888cf1521b5e9296b0219c
Tyler Adam Martinez <tyleradammartinez@Tylers-Mac-mini.attlocal.net>
2021-04-10 21:06:08 -0500

    Dynamic Date Picker based on current date, create a new plot to display more data (stock_plot)

--- main.py
+++ main.py
@@ -19,11 +19,20 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+from datetime import date
+today = date.today()
+import datetime
+last_year = datetime.datetime.now().year - 1
+this_month = datetime.datetime.now().month
+this_day = datetime.datetime.now().day
+today_last_year = datetime.datetime(last_year, this_month, this_day).strftime("%B %d, %Y")
+
 
 #TESTING DATA
-asset_dataframe = sd.stockData("googl", "2021-01-01", "2021-02-02")
-fig_plot = plt.plot(asset_dataframe.index , asset_dataframe['Close'])
-fig_candlestick = plt.candlesticks_plot(asset_dataframe)
+asset_dataframe = sd.stockData("googl", "2020-04-10", "2021-04-10")
+fig_price_plot = plt.plot(asset_dataframe.index, asset_dataframe['Close'])
+fig_stock_plot = plt.stock_plot(asset_dataframe.index, asset_dataframe)
+fig_candlestick = plt.candlesticks_plot(asset_dataframe, "Candlesticks Plot of Google")
 
 #Dashborad Layout
 app = dash.Dash()
@@ -49,11 +58,13 @@ app.layout = html.Div(
                     className="Input",
                     children=[
                         dcc.Input(
-                            id="asset_input"
+                            id="asset_input", 
+                            style={'width': '80%', 'height': '30px', 'padding': '0px', 'margin': '0px'}
                         ),
                         html.Button(
                             'Submit',
-                            id="asset_input_submit_btn"
+                            id="asset_input_submit_btn",
+                            style={'width': '19%', 'height': '35px'}
                         )                  
                     ]),
                 html.Div(
@@ -61,49 +72,69 @@ app.layout = html.Div(
                     children = [
                             dcc.DatePickerRange(
                             id="date_range",
-                            start_date_placeholder_text="Select a date",
-                            end_date="2021-03-03"
+                            is_RTL=True,
+                            display_format="MMMM DD, YYYY",
+                            start_date=today,
+                            end_date_placeholder_text=today_last_year, 
+                            style={'width': '100%', 'height': '30px'} 
                         )
                     ]
                 )                
             ],
             style={
                 'display':'grid',
-                'grid-template-columns':'auto auto auto'
+                'grid-template-columns':'1fr 1fr 1fr'
             }
         ),
+        html.Br(),
         dcc.Tabs(
             id="tabs",
             value="candlestick",
             children=[
                 dcc.Tab(label="Candlestick", value="candlestick"),
-                dcc.Tab(label="Simple Plot", value="simple_plot")
+                dcc.Tab(label="Price Plot", value="price_plot"),
+                dcc.Tab(label="Stock Plot", value="stock_plot")
             ]
         
         ),
         html.Div(id='tabs-content'),
-        html.Footer("Created By Tyler Adam Martinez and Svenn Mivedor",style={'text-align': 'center'})
+        html.Footer("Created By Tyler Adam Martinez and Svenn Mivedor",
+            style={
+                'text-align': 'center',
+                'bottom': '10px'
+            })
     ]
 )
 
-@app.callback(Output('tabs-content', 'children'), Input('tabs', 'value'))
+@app.callback(
+    Output('tabs-content', 'children'),
+    Input('tabs', 'value'), 
+)
 
 def render_content(tab):
     if tab == 'candlestick':
         return dcc.Graph(figure=fig_candlestick)
-    elif tab == 'simple_plot':
-        simple_plot_div = [
-            dcc.Checklist(
-                options=[
-                    {'label': 'Low', 'value': 'Low'},
-                    {'label': 'High', 'value': 'High'},
-                    {'label': 'Open', 'value': 'Open'},
-                    {'label': 'Close', 'value': 'Close', 'disabled': True}
-                ],
-                value=['Close'],
-                labelStyle={'display': 'inline-block'}
-            ),
-            dcc.Graph(figure=fig_plot)
+    elif tab == 'price_plot':
+        return dcc.Graph(figure=fig_price_plot)
+    elif tab == 'stock_plot':
+        stock_plot_div = [
+            html.Div(
+                id="stock_plot_div",
+                children=[
+                    dcc.Graph(figure=fig_stock_plot),
+                    dcc.Checklist(
+                        id="stock_plot_lines",
+                        options=[
+                            {'label': 'Low', 'value': 'Low'},
+                            {'label': 'High', 'value': 'High'},
+                            {'label': 'Open', 'value': 'Open'},
+                            {'label': 'Close', 'value': 'Close'}
+                        ],
+                        value=['Low', 'High', 'Open', 'Close'],
+                        labelStyle={'display': 'inline-block'}
+                    )                   
+                ]
+            )
         ]
-        return simple_plot_div
+        return stock_plot_div
 app.run_server(debug=True)
--- plotting.py
+++ plotting.py
@@ -6,15 +6,15 @@ from rich import print
 
 """""""""  Single Asset Plotting """""""""""
 
-def plot(price_array, time_array, plottitle = "Price of asset over time",  yaxistitle = 'Price (USD)', xaxistitle = 'Time (months)'):
+def plot(time_array, price_array, plottitle = "Price of asset over time",  yaxistitle = 'Price (USD)', xaxistitle = 'Time (months)'):
 	""" First parameter is for the price array and the second is for the time array"""
 	
 	#Creates the figure
 	fig = go.Figure(
 		data = [
 			go.Scatter(
-				x = price_array,
-				y = time_array, 
+				x = time_array,
+				y = price_array,
 			)
 		]
 	)
@@ -29,6 +29,44 @@ def plot(price_array, time_array, plottitle = "Price of asset over time",  yaxis
 	return fig
 	print("[bold red][Exiting\t][/bold red] plot\n")
 
+def stock_plot(time_array, stock_dataTable, plottitle = "Price of asset over time",  yaxistitle = 'Price (USD)', xaxistitle = 'Time (months)'):
+	""" First parameter is for the price array and the second is for the time array"""
+	
+	#Creates the figure
+	fig = go.Figure(
+		data = [
+			go.Scatter(
+				x = time_array,
+				y = stock_dataTable['Low'],
+				name="Low"
+			)
+		]
+	)
+	fig.add_scatter(
+		x = time_array, 
+		y = stock_dataTable['High'],
+		name="High"
+	)
+	fig.add_scatter(
+		x = time_array, 
+		y = stock_dataTable['Close'],
+		name="Close"
+	)
+	fig.add_scatter(
+		x = time_array, 
+		y = stock_dataTable['Open'],
+		name="Open"
+	)
+	fig.update_layout(
+		title = plottitle,
+		yaxis_title = yaxistitle,
+		xaxis_title = xaxistitle, 
+		template="plotly_dark"
+	)
+	print("[bold purple][Displaying\t][/bold purple] plot")
+	print(f"[bold yellow][Title:\t\t][/bold yellow] {plottitle}")
+	return fig
+	print("[bold red][Exiting\t][/bold red] plot\n")
 
 def candlesticks_plot(asset_dataframe, plottitle = "Price of asset over time",  yaxistitle = 'Price (USD)', xaxistitle = 'Time (months)'):
 	""" First parameter is for the asset date in pandas Datatable where the dates are the indexes """
